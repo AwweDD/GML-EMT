fe3356641988b841f89ab705e7728d31b3f05df1
AwweDD <liwangwang66@mail.nwpu.edu.cn>
2026-04-03 10:54:57 +0800

    Update example.py

--- GML/example.py
+++ GML/example.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     Datasets = ["NCT-CRC-HE-100K"]
     Models = ['densenet121', 'wide_resnet50_2']
     #
-    with open("/home/15t/lww/Data_paper2/pkl_test/f_" + Datasets[0] + "_" + str(ratio) + "_" + str(20000) + ".pkl", 'rb') as v:
+    with open("./Data/f_" + Datasets[0] + "_" + str(ratio) + "_" + str(0) + ".pkl", 'rb') as v:
         features = pickle.load(v)
-    with open("/home/15t/lww/Data_paper2/pkl_test/v_" + Datasets[0] + "_" + str(ratio) + "_" + str(20000) + ".pkl", 'rb') as f:
+    with open("./Data/v_" + Datasets[0] + "_" + str(ratio) + "_" + str(0) + ".pkl", 'rb') as f:
         variables = pickle.load(f)
     graph = GML.initial("example.config", variables, features, class_num)
     # inference
